fc955f88827cb8b81be2077a7c3f2e8f1385099a
Kevin Dam <kevinqdam@gmail.com>
2022-05-30 15:57:47 -0700

    Add positive unit test for decoding strings to Barcodes

--- test/unit/BarcodeCodec.test.ts
+++ test/unit/BarcodeCodec.test.ts
@@ -19,3 +19,12 @@ test('Decodes invalid barcodes to wrapped errors', () => {
   expect(decodeInput('123abc')).toBe(`Barcode value must be a numeric string (length from 6 through 12 characters), got: ${'123abc'}`);
   expect(decodeInput('a1b2c3d4')).toBe(`Barcode value must be a numeric string (length from 6 through 12 characters), got: ${'a1b2c3d4'}`);
 });
+
+test('Decodes valid barcodes to its literal string representation', () => {
+  expect(decodeInput('123456')).toBe('123456');
+  expect(decodeInput('1234567')).toBe('1234567');
+  expect(decodeInput('123456789')).toBe('123456789');
+  expect(decodeInput('1234567890')).toBe('1234567890');
+  expect(decodeInput('12345678901')).toBe('12345678901');
+  expect(decodeInput('123456789012')).toBe('123456789012');
+});
